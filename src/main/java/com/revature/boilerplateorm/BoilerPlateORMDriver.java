7c57b58bb4dd1385bff3535b00bbc02b0a433dac
Danhtran123 <47044112+Danhtran123@users.noreply.github.com>
2021-11-29 11:42:07 -0500

    fixed errors in Driver

--- src/main/java/com/revature/boilerplateorm/BoilerPlateORMDriver.java
+++ src/main/java/com/revature/boilerplateorm/BoilerPlateORMDriver.java
@@ -1,31 +1,16 @@
 package com.revature.boilerplateorm;
 
-import com.revature.boilerplateorm.daos.GenericDAO;
 import com.revature.boilerplateorm.daos.UserDAO;
-import com.revature.boilerplateorm.dtos.Credentials;
-import com.revature.boilerplateorm.dtos.Name;
 import com.revature.boilerplateorm.models.User;
 import com.revature.boilerplateorm.util.ConnectionFactory;
-import com.revature.boilerplateorm.util.QueryBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.List;
 
 public class BoilerPlateORMDriver {
 
-
-    /**
-     * From what I can tell...
-     * Use the annotations to retrieve the class,
-     * with that we can get the     class name      ->  table name
-     * We can also get the          class fields    ->  column names
-     *
-     * The last problem to solve is how to create the code, so we
-     * can insert these values into the query
-     */
     public static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
@@ -43,14 +28,6 @@ public class BoilerPlateORMDriver {
             User partialUser = new User();
             partialUser.setId(1);
             partialUser.setUsername("tester");
-
-            Credentials creds = new Credentials();
-            creds.setUsername("tester");
-            creds.setPassword("test");
-
-            Name name = new Name();
-            name.setFirstName("Danh");
-            name.setLastName("Tran");
 
             System.out.println(d.findByFirstNameAndLastName(User.class, "Test", "testerson"));
             System.out.println(d.findByUsernameAndPassword(User.class,"test", "test"));
